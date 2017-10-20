afa111ec94e55cbe8dca0eeb326a42860d6d5bfa
benn <benn@zhaw.ch>
2017-10-20 21:31:06 +0200

    Test

--- WebApp/Default.aspx.cs
+++ WebApp/Default.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace WebApp
@@ -11,6 +12,10 @@ namespace WebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            HtmlGenericControl d3jsInclude = new HtmlGenericControl("script");
+            d3jsInclude.Attributes.Add("type", "text/javascript");
+            d3jsInclude.Attributes.Add("src", "http://d3js.org/d3.v3.min.js");
+            Page.Header.Controls.Add(d3jsInclude);
 
         }
     }
